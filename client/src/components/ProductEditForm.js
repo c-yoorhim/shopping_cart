33eba6218e6264ce4c2dd31b982d5f47cdd9de68
Audry Hsu <audry.hsu@gmail.com>
2022-06-11 15:39:58 -0700

    formatting

--- client/src/components/ProductEditForm.js
+++ client/src/components/ProductEditForm.js
@@ -1,68 +1,89 @@
-import { useState } from "react"
-import { useDispatch } from 'react-redux'
-import { productEdited } from "../action/productsAction";
-import axios from 'axios'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { productEdited } from '../action/productsAction';
+import axios from 'axios';
 
-const ProductEditForm = ({ id, onCancel, onShowEditForm, product })=> {
-  const [productName, setProductName] = useState(product.title)
-  const [productPrice, setProductPrice] = useState(product.price)
-  const [productQuantity, setProductQuantity] = useState(product.quantity)
+const ProductEditForm = ({ id, onCancel, onShowEditForm, product }) => {
+  const [productName, setProductName] = useState(product.title);
+  const [productPrice, setProductPrice] = useState(product.price);
+  const [productQuantity, setProductQuantity] = useState(product.quantity);
   const dispatch = useDispatch();
 
   const handleHideForm = (e) => {
     e.preventDefault();
     onCancel();
-  } 
+  };
 
   const handleEdit = async (e) => {
-    e.preventDefault()
-      try {
-        const {data} = await axios.put(`api/products/${id}`, {
-          title: productName,
-          quantity: productQuantity,
-          price: productPrice
-        })
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(`api/products/${id}`, {
+        title: productName,
+        quantity: productQuantity,
+        price: productPrice,
+      });
 
-        dispatch(productEdited(data))
+      dispatch(productEdited(data));
 
-        resetForm();
-        onShowEditForm();
-      } catch (e) { console.error(e); }
-  }
+      resetForm();
+      onShowEditForm();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const resetForm = () => {
-    setProductName("")
-    setProductPrice("")
-    setProductQuantity("")
-    onShowEditForm()
-  }
-  
+    setProductName('');
+    setProductPrice('');
+    setProductQuantity('');
+    onShowEditForm();
+  };
+
   return (
     <div>
-    <h3>Edit Product</h3>
-    <form>
-      <div className="input-group">
-        <label htmlFor="product-name">Product Name</label>
-        <input type="text" id="product-name" value={productName} onChange={event=>setProductName(event.target.value)}/>
-      </div>
+      <h3>Edit Product</h3>
+      <form>
+        <div className="input-group">
+          <label htmlFor="product-name">Product Name</label>
+          <input
+            type="text"
+            id="product-name"
+            value={productName}
+            onChange={(event) => setProductName(event.target.value)}
+          />
+        </div>
 
-      <div className="input-group">
-        <label htmlFor="product-price">Price</label>
-        <input type="text" id="product-price" value={productPrice} onChange={event=>setProductPrice(event.target.value)}/>
-      </div>
+        <div className="input-group">
+          <label htmlFor="product-price">Price</label>
+          <input
+            type="text"
+            id="product-price"
+            value={productPrice}
+            onChange={(event) => setProductPrice(event.target.value)}
+          />
+        </div>
 
-      <div className="input-group">
-        <label htmlFor="product-quantity">Quantity</label>
-        <input type="text" id="product-quantity" value={productQuantity} onChange={event=>setProductQuantity(event.target.value)}/>
-      </div>
+        <div className="input-group">
+          <label htmlFor="product-quantity">Quantity</label>
+          <input
+            type="text"
+            id="product-quantity"
+            value={productQuantity}
+            onChange={(event) => setProductQuantity(event.target.value)}
+          />
+        </div>
 
-      <div className="actions form-actions">
-        <a href="/#" className="button" onClick={ handleEdit }>Edit</a>
-        <a href="/#" className="button" onClick={ handleHideForm }>Cancel</a>
-      </div>
-    </form> 
+        <div className="actions form-actions">
+          <a href="/#" className="button" onClick={handleEdit}>
+            Edit
+          </a>
+          <a href="/#" className="button" onClick={handleHideForm}>
+            Cancel
+          </a>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ProductEditForm
+export default ProductEditForm;
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,41 +1,49 @@
-
-import Product from './Product'
-import ProductAddForm from './ProductAddForm'
+import Product from './Product';
+import ProductAddForm from './ProductAddForm';
 import { useDispatch, useSelector } from 'react-redux';
-import toggleAddForm from "../action/toggleAddFormAction";
+import toggleAddForm from '../action/toggleAddFormAction';
 import { useEffect } from 'react';
 // import { productsReceived } from "../action/productsAction"
-import {fetchProducts } from "../features/products"
+import { fetchProducts } from '../features/products';
 
-const Products = ()=> {
+const Products = () => {
   const dispatch = useDispatch();
-  const products = useSelector((state)=> state.products)
-  const addFormVisibility = useSelector((state) => state.addFormVisibility)
+  const products = useSelector((state) => state.products);
+  const addFormVisibility = useSelector((state) => state.addFormVisibility);
 
   useEffect(() => {
-      // dispatch the thunk function instead of action object
-    dispatch(fetchProducts())
-  },[dispatch]);
+    // dispatch the thunk function instead of action object
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
   const handleAddClick = (e) => {
-    e.preventDefault()
-    dispatch(toggleAddForm(!addFormVisibility))
-  }
- 
+    e.preventDefault();
+    dispatch(toggleAddForm(!addFormVisibility));
+  };
+
   return (
     <main>
       <div className="product-listing">
         <h2>Products</h2>
-        {products.map(product => {
-          return (<Product key={product._id} product={product}/>)
+        {products.map((product) => {
+          return <Product key={product._id} product={product} />;
         })}
       </div>
-      
+
       <div className="add-form">
-        <p><a href="/#" className="button add-product-button" onClick={ handleAddClick }>Add A Product</a></p>
+        <p>
+          <a
+            href="/#"
+            className="button add-product-button"
+            onClick={handleAddClick}
+          >
+            Add A Product
+          </a>
+        </p>
       </div>
       {addFormVisibility ? <ProductAddForm /> : null}
     </main>
-)}
+  );
+};
 
 export default Products;
